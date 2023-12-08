f6cbbc1a81f939610b5fa44339c483ebe3d17bbf
lisitsinyr <lisitsinyr@gmail.com>
2023-12-08 19:13:03 +0400

    Main

--- main.py
+++ main.py
@@ -23,26 +23,59 @@ import datetime
 #------------------------------------------
 # БИБЛИОТЕКИ сторонние
 #------------------------------------------
+import pandas
+
 
 #------------------------------------------
 # БИБЛИОТЕКИ LU
 #------------------------------------------
 import LULog
-import LUFile
-import LUProc
-import LUos
-import LUObjectsYT
-import LUDateTime
-import LUStrUtils
-import LUDecotators
-import LUSheduler
-import LUConsole
+# import LUFile
+# import LUProc
+# import LUos
+# import LUObjectsYT
+# import LUDateTime
+# import LUStrUtils
+# import LUDecotators
+# import LUSheduler
+# import LUConsole
+
+#------------------------------------------
+# const
+#------------------------------------------
+sFileName = 'G:\\SOFT-install\\03 ЮРА_ТЕЛЕФОН\\STORE\\НАСТРОЙКА\\ЯРЛЫКИ\\HM20.xlsx'
 
 #------------------------------------------
 #
 #------------------------------------------
 def main ():
 #beginfunction
+    # название электронной таблицы
+    # Если в качестве значения указать список, то будет возвращён словарь датафреймов
+    sheet_name = 0
+    sheet_name = 'Лист1'
+    # сколько строк вы хотите пропустить
+    skiprows = None
+    skiprows = 1
+    # При установленном значении “0” используется первая строка.
+    # Если у столбцов не должно быть заголовков, установите значение “None”
+    header = 0
+    header = 1
+    # список с именами столбцов
+    names = None
+    # список с номерами или названиями столбцов.
+    usecols = None
+    #
+    dtype = None
+    nrows = None
+
+    APPs = pandas.read_excel(sFileName)
+
+    # APPs = pandas.read_excel(sFileName, sheet_name = 'Лист1',
+    #                          skiprows = 1, header = 1,
+    #                          names = None, usecols = None, dtype = None,
+    #                          nrows = None)
+
     LResult = 0
     s = 'ExitProgram...'
     LULog.LoggerAPPS.info (s)
